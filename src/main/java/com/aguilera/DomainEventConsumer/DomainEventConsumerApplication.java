ec4b07e97394c75d1ae94c23789865eefd3bcb53
tony.aguilera <antonio.aguilera@adevinta.com>
2025-02-11 22:19:47 +0100

    first commit

---
+++ src/main/java/com/aguilera/DomainEventConsumer/DomainEventConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.aguilera.DomainEventConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainEventConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainEventConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aguilera/DomainEventConsumer/DomainEventConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguilera.DomainEventConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainEventConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
